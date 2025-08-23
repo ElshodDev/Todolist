465e1fdb5ec0953398c2e9e8143ab161ab2a253c
Elshod <elshodibadullayev28@gmail.com>
2025-08-24 00:02:40 +0100

    INFRA: Initialize Build Project

---
+++ Todolist.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Todolist Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "main" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "main" }
+        }
+    },
+
+    Jobs = new Dictionary<string, Job>
+    {
+        ["build"] = new Job
+        {
+            RunsOn = BuildMachines.Windows2022,
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Seting Up .NET",
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "9.0.304"
+                    }
+                },
+                new RestoreTask
+                {
+                    Name = "Restoring Nuget Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "building Project"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Tests"
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/dotnet.yml");
