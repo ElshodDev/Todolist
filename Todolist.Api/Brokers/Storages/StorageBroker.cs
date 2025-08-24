68af7a9f9c3dea7ea82b4301c6ed3c4bf33d91bc
Elshod <elshodibadullayev28@gmail.com>
2025-08-24 02:24:08 +0100

    DATA: Create TaskItem Model and migrate

---
+++ Todolist.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,24 @@
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Todolist.Api.Brokers.Storages
+{
+    public partial class StorageBroker : EFxceptionsContext
+    {
+        private readonly IConfiguration configuration;
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectingString = this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectingString);
+        }
+
+        public override void Dispose() { }
+
+    }
+}
