12916564a02db7ab8ab0f03b6546c7554353a136
Elshod <elshodibadullayev28@gmail.com>
2025-08-24 10:06:02 +0100

    BROKERS: Insert TaskItem

---
+++ Todolist.Api/Brokers/Storages/IStorageBroker.Taskitems.cs
@@ -0,0 +1,9 @@
+﻿using Todolist.Api.Models.Foundations.TaskItems;
+
+namespace Todolist.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<TaskItem> InsertTaskItemAsync(TaskItem taskItem);
+    }
+}
