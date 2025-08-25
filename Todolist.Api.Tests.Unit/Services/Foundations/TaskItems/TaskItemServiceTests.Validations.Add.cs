5e888929af396517cd2c749511a7e7587c27d2dd
Elshod <elshodibadullayev28@gmail.com>
2025-08-25 14:28:37 +0100

    public async Task ShouldThrowValidationExceptionOnAddIfTaskItemIsinvalidAndlogitAsync() -> FAIL

--- Todolist.Api.Tests.Unit/Services/Foundations/TaskItems/TaskItemServiceTests.Validations.Add.cs
+++ Todolist.Api.Tests.Unit/Services/Foundations/TaskItems/TaskItemServiceTests.Validations.Add.cs
@@ -38,17 +38,74 @@ namespace Todolist.Api.Tests.Unit.Services.Foundations.TaskItems
                     expectedTaskItemValidationException))),
                 Times.Once);
 
-            this.storagebrokerMock.Verify(broker=>
+            this.storagebrokerMock.Verify(broker =>
             broker.InsertTaskItemAsync(It.IsAny<TaskItem>()),
             Times.Never);
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storagebrokerMock.VerifyNoOtherCalls();
         }
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfTaskItemIsinvalidAndlogitAsync(
+            string invalidText)
+        {
+            //given 
+            var invalidTaskItem = new TaskItem
+            {
+                Id = Guid.NewGuid(),
+                Title = invalidText,
+                CreatedAt = default
+            };
+
+            var invalidTaskItemException = new InvalidTaskItemException();
+
+            invalidTaskItemException.AddData(
+                nameof(TaskItem.Id),
+                values: "Id is Required");
+
+            invalidTaskItemException.AddData(
+              nameof(TaskItem.Title),
+              values: "Text is Required");
+
+            invalidTaskItemException.AddData(
+                nameof(TaskItem.CreatedAt),
+                values: "Date is required");
+
+            var expectedTaskItemValidationException =
+            new TaskItemValidationException(invalidTaskItemException);
+
+            //when
+            ValueTask<TaskItem> addTaskItemTask =
+        this.taskItemService.AddTaskItemAsync(invalidTaskItem);
+
+            //then
+            await Assert.ThrowsAsync<TaskItemValidationException>(() =>
+            addTaskItemTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogError(It.Is(SameExceptionAs(
+                expectedTaskItemValidationException))),
+                Times.Once());
+
+            this.storagebrokerMock.Verify(Broker =>
+            Broker.InsertTaskItemAsync(It.IsAny<TaskItem>()),
+            Times.Never());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storagebrokerMock.VerifyNoOtherCalls();
+        }
+
+
+
 
         private Expression<Func<Exception, bool>> SameExceptionAs(
             Xeption expectedTaskItemValidationException) =>
             actualtaskItem =>
             actualtaskItem.SameExceptionAs(expectedTaskItemValidationException);
+
+
     }
 }
